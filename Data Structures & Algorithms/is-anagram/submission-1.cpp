836aea2aaa065ed32b8f95388a9cffbe2515b7a9
Akshit Singh <khush227799@gmail.com>
2026-07-22 22:14:04 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        unordered_map<char,int> mp;
+        for(auto ch:s){
+            mp[ch]++;
+        }
+        for(auto ch:t){
+            mp[ch]--;
+            if(mp[ch]<0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
